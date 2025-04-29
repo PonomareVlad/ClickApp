5e6f59b16f8fc3257972366bba8b002c466d43f8
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2025-04-29 12:44:38 +0200

    Create bot.mjs

---
+++ src/bot.mjs
@@ -0,0 +1,10 @@
+import { Bot } from 'grammy'
+
+export const {
+    TELEGRAM_BOT_TOKEN: token,
+    TELEGRAM_SECRET_TOKEN: secretToken = String(token).split(':').pop(),
+} = process.env
+
+export const bot = new Bot(token)
+
+bot.on('message:text', ctx => ctx.reply(ctx.msg.text))
